d99e74c1a68d167608e66dd59b409db143014253
Tobias Burger <tobias_burger@hotmail.com>
2014-02-22 18:41:49 +0100

    added console sample

---
+++ BankOCRKata.Console/Program.fs
@@ -0,0 +1,36 @@
+﻿open BankOCRKata
+open System.IO
+
+let readSampleFile filename = 
+    seq { 
+        let lines = File.ReadAllLines filename
+        
+        let getDigitChunks (s : string) = 
+            [ for i in [ 0..3..78 ] -> s.[i..i + 2] ]
+        
+        let getDigits (xs : string list) = 
+            [ for i in [ 0..9 - 1 ] -> xs.[i] + xs.[i + 9] + xs.[i + 18] ]
+        
+        for i in [ 0..4..lines.Length - 1 ] -> 
+            lines.[i] + lines.[i + 1] + lines.[i + 2]
+            |> getDigitChunks
+            |> getDigits
+            |> String.concat ""
+    }
+
+[<EntryPoint>]
+let main argv = 
+
+    // parse number after number
+    //readSampleFile "sample.txt"
+    //|> Seq.map AccountNumber.parse
+    //|> Seq.iter (printfn "%O")
+    // parse in parallel
+
+    readSampleFile "sample.txt"
+    |> Seq.map (async.Return << AccountNumber.parse)
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Seq.iter (printfn "%O")
+
+    0
