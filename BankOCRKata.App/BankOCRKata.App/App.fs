49864ede58689718fe20264d16375f50f75284ee
Tobias Burger <tobias_burger@hotmail.com>
2014-02-23 19:03:06 +0100

    added WPF application just for fun

---
+++ BankOCRKata.App/BankOCRKata.App/App.fs
@@ -0,0 +1,38 @@
+﻿module MainApp
+
+open System
+open System.Windows
+open System.Windows.Controls
+open FSharpx
+
+type MainWindow = XAML< "MainWindow.xaml" >
+
+type Result = 
+    { Original : string
+      Parsed : BankOCRKata.AccountNumber }
+
+let loadWindow() = 
+    let window = MainWindow()
+    let invokeOnUI f = window.Root.Dispatcher.BeginInvoke(Action(f)) |> ignore
+    window.parse.Click.Add(fun _ -> 
+        window.results.Items.Clear()
+        Utils.readSampleFile "sample.txt"
+//        |> Seq.iter (fun (orig, s) ->
+//            let an = BankOCRKata.AccountNumber.parse s
+//            let res = { Original = orig; Parsed = an }
+//            window.results.Items.Add res |> ignore
+//        ))
+        |> Seq.map (fun res -> 
+            let (orig, s) = res
+            async {
+                let an = BankOCRKata.AccountNumber.parse s
+                let res = { Original = orig; Parsed = an }
+                invokeOnUI (fun _ -> window.results.Items.Add(res) |> ignore)
+            })
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> ignore)
+    window.Root
+
+[<STAThread>]
+(new Application()).Run(loadWindow()) |> ignore
---
+++ BankOCRKata.App/BankOCRKata.App/Utils.fs
@@ -0,0 +1,41 @@
+﻿module Utils
+
+open System.IO
+open Microsoft.FSharp.Control
+
+module Seq = 
+    let chunk n xs = 
+        seq { 
+            let i = ref 0
+            let arr = ref <| Array.create n (Unchecked.defaultof<'a>)
+            for x in xs do
+                if !i = n then 
+                    yield !arr
+                    arr := Array.create n (Unchecked.defaultof<'a>)
+                    i := 0
+                (!arr).[!i] <- x
+                i := !i + 1
+            if !i <> 0 then yield (!arr).[0..!i - 1]
+        }
+
+let readSampleFile filename = seq {
+    let lines = File.ReadLines filename
+
+    let digitLineSize = 4
+        
+    let getDigitChunks (s : string) = 
+        [ for i in [ 0..3..78 ] -> s.[i..i + 2] ]
+        
+    let getDigits (xs : string list) = 
+        [ for i in [ 0..9 - 1 ] -> xs.[i] + xs.[i + 9] + xs.[i + 18] ]
+        
+    yield! lines
+    |> Seq.chunk digitLineSize
+    |> Seq.map (fun window ->
+        String.concat "\n" window.[0..2],
+        window.[0] + window.[1] + window.[2]
+        |> getDigitChunks
+        |> getDigits
+        |> String.concat ""
+    )
+}
