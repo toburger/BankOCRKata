8e52659c36d40e32227274d5a1b9ae4ae2db3666
Tobias Burger <tobias_burger@hotmail.com>
2014-02-24 15:02:24 +0100

    added progress dialog and removed progress ring

--- BankOCRKata.App/App.fs
+++ BankOCRKata.App/App.fs
@@ -15,12 +15,23 @@ type Result =
 
 type MetroWindow with
     
-    member self.AsyncShowMessage(title, message) = 
+    member self.AsyncShowMessage(title, message) =
         self.ShowMessageAsync(title, message)
         |> Async.AwaitTask
         |> Async.Ignore
+
+    member self.AsyncShowProgress(title, message) =
+        self.ShowProgressAsync(title, message)
+        |> Async.AwaitTask
     
     member self.InvokeOnUI f = self.Dispatcher.BeginInvoke(Action(f)) |> ignore
+
+type ProgressDialogController with
+
+    member self.AsyncClose() =
+        if self.IsOpen
+        then self.CloseAsync() |> Async.AwaitIAsyncResult |> Async.Ignore
+        else async.Return()
 
 let loadWindow() = 
     let window = MainWindow()
@@ -36,22 +47,25 @@ let loadWindow() =
             })
         |> Async.Parallel
         |> Async.Ignore
-        |> Async.StartImmediate
-        |> ignore
 
     self.DragEnter.Add(fun e -> e.Effects <- DragDropEffects.Copy)
 
-    self.Drop.Add(fun e -> Async.StartImmediate <| async { 
-        if e.Data.GetDataPresent(DataFormats.FileDrop) then 
-            window.progress.IsActive <- true
+    self.Drop.Add(fun e -> Async.StartImmediate <| async {
+        if e.Data.GetDataPresent(DataFormats.FileDrop) then
             window.results.Items.Clear()
             let files = e.Data.GetData(DataFormats.FileDrop) :?> string []
+            let! progress = self.AsyncShowProgress("just a second!", "")
             for file in files do
                 try 
-                    parse (fun res -> window.results.Items.Add(res) |> ignore) file
-                with _ -> 
+                    progress.SetProgress(0.5)
+                    progress.SetMessage(sprintf "I'm parsing the file: %s" file)
+                    do! parse (fun res -> window.results.Items.Add(res) |> ignore) file
+                    do! Async.Sleep 500
+                with _ ->
+                    do! Async.Sleep 500
+                    do! progress.AsyncClose()
                     do! self.AsyncShowMessage("Error", (sprintf "Error while parsing the file: %s." file))
-            window.progress.IsActive <- false
+            do! progress.AsyncClose()
     })
 
     self
