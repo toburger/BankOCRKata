296e7479c991d40654745c55c24aaed390e4e318
Tobias Burger <tobias_burger@hotmail.com>
2014-02-24 13:43:47 +0100

    refactoring

--- BankOCRKata.App/App.fs
+++ BankOCRKata.App/App.fs
@@ -4,6 +4,7 @@ open System
 open System.Windows
 open System.Windows.Controls
 open FSharpx
+open MahApps.Metro.Controls
 open MahApps.Metro.Controls.Dialogs
 
 type MainWindow = XAML< "MainWindow.xaml" >
@@ -12,41 +13,48 @@ type Result =
     { Original : string
       Parsed : BankOCRKata.AccountNumber }
 
+type MetroWindow with
+    
+    member self.AsyncShowMessage(title, message) = 
+        self.ShowMessageAsync(title, message)
+        |> Async.AwaitTask
+        |> Async.Ignore
+    
+    member self.InvokeOnUI f = self.Dispatcher.BeginInvoke(Action(f)) |> ignore
+
 let loadWindow() = 
     let window = MainWindow()
 
-    let invokeOnUI f = window.Root.Dispatcher.BeginInvoke(Action(f)) |> ignore
-
-    let parse onAdd file =
+    let self = window.Root
+    
+    let parse onAdd file = 
         Utils.readSampleFile file
-        |> Seq.map (fun (orig, s) ->
-            async {
+        |> Seq.map (fun (orig, s) -> 
+            async { 
                 let an = BankOCRKata.AccountNumber.parse s
-                invokeOnUI (fun _ -> onAdd({ Original = orig; Parsed = an }))
+                self.InvokeOnUI(fun _ -> onAdd ({ Original = orig; Parsed = an }))
             })
         |> Async.Parallel
         |> Async.Ignore
         |> Async.StartImmediate
         |> ignore
 
-    window.results.DragEnter.Add(fun e -> e.Effects <- DragDropEffects.Copy)
+    self.DragEnter.Add(fun e -> e.Effects <- DragDropEffects.Copy)
 
-    let asyncShowMessage title message = window.Root.ShowMessageAsync(title, message) |> Async.AwaitTask |> Async.Ignore
-
-    window.results.Drop.Add(fun e -> Async.StartImmediate <| async {
-        if e.Data.GetDataPresent(DataFormats.FileDrop) then
+    self.Drop.Add(fun e -> Async.StartImmediate <| async { 
+        if e.Data.GetDataPresent(DataFormats.FileDrop) then 
             window.progress.IsActive <- true
             window.results.Items.Clear()
-            let files = e.Data.GetData(DataFormats.FileDrop) :?> string[]
+            let files = e.Data.GetData(DataFormats.FileDrop) :?> string []
             for file in files do
-                try
+                try 
                     parse (fun res -> window.results.Items.Add(res) |> ignore) file
-                with _ ->
-                    do! asyncShowMessage "Error" (sprintf "Error while parsing the file: %s." file)
+                with _ -> 
+                    do! self.AsyncShowMessage("Error", (sprintf "Error while parsing the file: %s." file))
             window.progress.IsActive <- false
     })
 
-    window.Root
+    self
 
 [<STAThread>]
 (new Application()).Run(loadWindow()) |> ignore
