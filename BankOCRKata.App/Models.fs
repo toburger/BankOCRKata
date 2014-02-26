46f4fbc3a0b2b8b80e8d6074053c232f65c72c86
Tobias Burger <tobias_burger@hotmail.com>
2014-02-26 16:00:38 +0100

    now on a working state

---
+++ BankOCRKata.App/Models.fs
@@ -0,0 +1,39 @@
+﻿namespace Models
+
+open System
+
+[<CustomComparison; CustomEquality>]
+type Result =
+    { Original : string
+      Parsed : BankOCRKata.AccountNumber }
+    interface IComparable with
+        member self.CompareTo(other) =
+            let res =
+                match other with
+                | :? Result as other -> compare self.Parsed other.Parsed
+                | _ -> -1
+            res
+    override self.Equals(other) =
+        let res =
+            match other with
+            | :? Result as other -> self.Original = other.Original && self.Parsed = other.Parsed
+            | _ -> false
+        res
+    override self.GetHashCode() = hash self.Original ^^^ hash self.Parsed
+
+module AccountNumberParser =
+    let parse cache onAdd file =
+    
+        let parse = BankOCRKata.AccountNumber.parse
+        let parsememoized = Utils.memoize parse
+    
+        Utils.readSampleFile file
+        |> Seq.map (fun (orig, s) -> 
+            async {
+                let an =
+                    if cache
+                    then parsememoized s
+                    else parse s
+                onAdd { Original = orig; Parsed = an }
+            })
+        |> Async.Parallel
--- BankOCRKata.App/ViewModels.fs
+++ BankOCRKata.App/ViewModels.fs
@@ -4,11 +4,66 @@ type IShell = interface end
 
 namespace ViewModels
 
+open Shell
+open Models
+
 open System.ComponentModel.Composition
 open Caliburn.Micro
-open Shell
+open System
+open System.Windows
+open System.Windows.Controls
+open System.Windows.Data
 
 [<Export(typeof<IShell>)>]
 type ShellViewModel [<ImportingConstructor>] (windowManager: IWindowManager) =
     inherit Screen()
+    
+    let setPosition e = ()
+
     interface IShell
+
+    member val PreviewText: string = null with get, set
+    member val PreviewVisibility: Visibility = Visibility.Collapsed with get, set
+    member val PreviewPosition: Point = Point(0.0, 0.0) with get, set
+    member val ParsedAccountNumbers: BindableCollection<Result> = BindableCollection<Result>() with get, set
+    member val CachedParsing: bool = true with get, set
+
+    member self.OnDragEnter (e: DragEventArgs) =
+        if e.Data.GetDataPresent(DataFormats.FileDrop) then 
+            let file = e.Data.GetData(DataFormats.FileDrop) :?> string [] |> Seq.head
+            self.PreviewText <- null
+            setPosition e
+            self.PreviewVisibility <- Visibility.Visible
+            async {
+                let preview = Utils.readPreviewOfFile 11 file
+                Execute.BeginOnUIThread
+                    (fun _ -> self.PreviewText <- preview)
+            }
+            |> Async.Start
+
+    member self.OnDragOver (e: DragEventArgs) = setPosition e
+
+    member self.OnDragLeave (e: RoutedEventArgs) = self.PreviewVisibility <- Visibility.Collapsed
+
+    member self.OnDrop (e: DragEventArgs) = Async.StartImmediate <| async {
+        self.PreviewVisibility <- Visibility.Collapsed
+
+        if e.Data.GetDataPresent(DataFormats.FileDrop) then
+            self.ParsedAccountNumbers.Clear()
+            let files = e.Data.GetData(DataFormats.FileDrop) :?> string []
+//            let! progress = self.AsyncShowProgress("just a second!", "")
+            for file in files do
+                try
+//                    progress.SetProgress(0.5)
+//                    progress.SetMessage(sprintf "I'm parsing the file: %s" file)
+                    let! res = AccountNumberParser.parse self.CachedParsing (fun res -> Execute.BeginOnUIThread(fun _ -> self.ParsedAccountNumbers.Add res)) file
+//                    do! self.AsyncShowMessage("yeeehaw!", sprintf "%d Account numbers where parsed." res.Length)
+//                    do! Async.Sleep 500
+                    ()
+                with _ ->
+                    ()
+//                    do! Async.Sleep 500
+//                    do! progress.AsyncClose()
+//                    do! self.AsyncShowMessage("uuups!", (sprintf "Error while parsing the file: %s." file))
+//            do! progress.AsyncClose()
+    }
