e02b38c50300c69e6e44c06ebcbedd7db1dcf654
Tobias Burger <tobias_burger@hotmail.com>
2016-02-18 10:26:48 +0100

    and another try for travis

--- build.sh
+++ build.sh
@@ -14,10 +14,10 @@ then
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
-  
+
   [ ! -e build.fsx ] && .paket/paket.exe update
-  [ ! -e build.fsx ] && packages/FAKE/tools/FAKE.exe init.fsx
-  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+  [ ! -e build.fsx ] && packages/build/FAKE/tools/FAKE.exe init.fsx
+  packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 else
   # use mono
   mono .paket/paket.bootstrapper.exe
@@ -33,6 +33,6 @@ else
   fi
 
   [ ! -e build.fsx ] && mono .paket/paket.exe update
-  [ ! -e build.fsx ] && mono packages/FAKE/tools/FAKE.exe init.fsx
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+  [ ! -e build.fsx ] && mono packages/build/FAKE/tools/FAKE.exe init.fsx
+  mono packages/build/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
